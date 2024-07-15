3051348404195eeb0d4216038589b5fa9d11a608
Francesco <1682099+francescostella@users.noreply.github.com>
2024-07-15 12:07:29 +0200

    refactor: render board programmatically

--- boards.py
+++ boards.py
@@ -38,13 +38,13 @@ class Board:
             self.draw_grid(is_debug = True)
     
     def draw_grid(self, is_debug = False):
-        print(
-f"""
-    |   A   |   B   |   C   |   D   |
-   1|  {self.items[0]['A'].render(is_debug)}  |  {self.items[0]['B'].render(is_debug)}  |  {self.items[0]['C'].render(is_debug)}  |  {self.items[0]['D'].render(is_debug)}  |
-   2|  {self.items[1]['A'].render(is_debug)}  |  {self.items[1]['B'].render(is_debug)}  |  {self.items[1]['C'].render(is_debug)}  |  {self.items[1]['D'].render(is_debug)}  |
-   3|  {self.items[2]['A'].render(is_debug)}  |  {self.items[2]['B'].render(is_debug)}  |  {self.items[2]['C'].render(is_debug)}  |  {self.items[2]['D'].render(is_debug)}  |
-   4|  {self.items[3]['A'].render(is_debug)}  |  {self.items[3]['B'].render(is_debug)}  |  {self.items[3]['C'].render(is_debug)}  |  {self.items[3]['D'].render(is_debug)}  |
-"""
-    )
+        counter = 1
+        print("    |   A   |   B   |   C   |   D   |")
+        for row in self.items:
+            print_row = f"   {counter}|"
+            for key, card in row.items():
+                print_row += f"  {card.render(is_debug)}  |"
+            print(f"{print_row}")
+            counter += 1
+        print("\n")
     
